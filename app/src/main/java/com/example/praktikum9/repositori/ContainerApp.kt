390908a2ef640dfa44871dbc4927eb92de8e39f4
mauraaninditakirana <kiranamaura351@gmail.com>
2025-11-27 13:19:05 +0700

    membuat container app dan repositori siswa

---
+++ app/src/main/java/com/example/praktikum9/repositori/ContainerApp.kt
@@ -0,0 +1,21 @@
+package com.example.praktikum9.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.praktikum9.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+    ContainerApp{
+        override val repositoriSiswa: RepositoriSiswa by lazy {
+        OfflineRepositoriSiswa(
+            DatabaseSiswa.getDatabase(context).siswaDao())
+        }
+    }
+
+    class AplikasiSiswa : Application() {
+
+    }
